358c6ab4b4c7ffb9548147fc8ca89493d42d9683
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-06 22:22:24 +0200

    updated sensitivity and sensitivity_parallel scripts to use DAISIEprep::sensitivity()

--- scripts/sensitivity.R
+++ scripts/sensitivity.R
@@ -1,17 +1,4 @@
-parameter_space <- data.frame(
-  extraction_method = "min",
-  asr_method = NA_character_,
-  tie_preference = NA_character_
-)
-
-parameter_space <- rbind(
-  parameter_space,
-  expand.grid(
-    extraction_method = c("asr"),
-    asr_method = c("parsimony", "mk"),
-    tie_preference = c("island", "mainland")
-  )
-)
+# script to run the sensitivity analysis
 
 # load madagascar mammals species data table
 data("madagascar_mammals", package = "DAISIEprepExtra")
@@ -26,136 +13,32 @@ phylos_complete <- ape::read.nexus(file = system.file(
   package = "DAISIEprepExtra"
 ))
 
-# convert trees to phylo4 objects
-phylos_dna <- lapply(phylos_dna, phylobase::phylo4)
-phylos_complete <- lapply(phylos_complete, phylobase::phylo4)
-
-# create endemicity status data frame
-endemicity_status_dna <- lapply(
+sensitivity_dna <- lapply(
   phylos_dna,
-  DAISIEprep::create_endemicity_status,
-  island_species = madagascar_mammals
+  DAISIEprep::sensitivity,
+  island_species = madagascar_mammals,
+  extraction_method = c("min", "asr"),
+  asr_method = c("parsimony", "mk"),
+  tie_preference = c("island", "mainland"),
+  island_age = c(5),
+  num_mainland_species = c(1000)
 )
 
-endemicity_status_complete <- lapply(
+sensitivity_complete <- lapply(
   phylos_complete,
-  DAISIEprep::create_endemicity_status,
-  island_species = madagascar_mammals
+  DAISIEprep::sensitivity,
+  island_species = madagascar_mammals,
+  extraction_method = c("min", "asr"),
+  asr_method = c("parsimony", "mk"),
+  tie_preference = c("island", "mainland"),
+  island_age = c(5),
+  num_mainland_species = c(1000)
 )
 
-# combine tree and endemicity status
-multi_phylods_dna <- list()
-multi_phylods_complete <- list()
-for (i in seq_along(phylos_dna)) {
-  message("Converting phylo ", i, " of ", length(phylos_dna))
-  multi_phylods_dna[[i]] <- phylobase::phylo4d(
-    phylos_dna[[i]],
-    endemicity_status_dna[[i]]
-  )
-  multi_phylods_complete[[i]] <- phylobase::phylo4d(
-    phylos_complete[[i]],
-    endemicity_status_complete[[i]]
-  )
-}
-
-ml_list <- list()
-for (i in seq_len(nrow(parameter_space))) {
-
-  message("Parameter set: ", i, " of ", nrow(parameter_space))
-
-  if (parameter_space$extraction_method[i] == "asr") {
-    multi_phylods_dna <- lapply(
-      multi_phylods_dna,
-      DAISIEprep::add_asr_node_states,
-      asr_method = parameter_space$asr_method[i]
-    )
-
-    multi_phylods_complete <- lapply(
-      multi_phylods_complete,
-      DAISIEprep::add_asr_node_states,
-      asr_method = parameter_space$asr_method[i]
-    )
-  }
-
-  # extract island community
-  multi_island_tbl_dna <- DAISIEprep::multi_extract_island_species(
-    multi_phylod = multi_phylods_dna,
-    extraction_method = parameter_space$extraction_method[i],
-    verbose = TRUE
-  )
-
-  multi_island_tbl_complete <- DAISIEprep::multi_extract_island_species(
-    multi_phylod = multi_phylods_complete,
-    extraction_method = parameter_space$extraction_method[i],
-    verbose = TRUE
-  )
-
-  # convert to daisie data table
-  daisie_datatable_dna <- lapply(
-    multi_island_tbl_dna,
-    DAISIEprep::as_daisie_datatable,
-    island_age = 88
-  )
-
-  daisie_datatable_complete <- lapply(
-    multi_island_tbl_complete,
-    DAISIEprep::as_daisie_datatable,
-    island_age = 88
-  )
-
-  # convert to daisie data list
-  daisie_data_list_dna <- lapply(
-    daisie_datatable_dna,
-    DAISIEprep::create_daisie_data,
-    island_age = 88,
-    num_mainland_species = 1000
-  )
-
-  daisie_data_list_complete <- lapply(
-    daisie_datatable_complete,
-    DAISIEprep::create_daisie_data,
-    island_age = 88,
-    num_mainland_species = 1000
-  )
-
-  # fit DAISIE model to data
-  ml_dna <- list()
-  ml_complete <- list()
-  for (j in seq_along(phylos_dna)) {
-    message(
-      "Fitting DAISIE model to data set ",
-      j,
-      " of ",
-      length(daisie_data_list_dna)
-    )
-
-    ml_dna[[j]] <- DAISIE::DAISIE_ML_CS(
-      datalist = daisie_data_list_dna[[j]],
-      initparsopt = c(1, 1, 200, 0.1, 1),
-      idparsopt = 1:5,
-      parsfix = NULL,
-      idparsfix = NULL,
-      ddmodel = 11,
-      jitter = 1e-5
-    )
-
-    ml_complete[[j]] <- DAISIE::DAISIE_ML_CS(
-      datalist = daisie_data_list_complete[[j]],
-      initparsopt = c(1, 1, 200, 0.1, 1),
-      idparsopt = 1:5,
-      parsfix = NULL,
-      idparsfix = NULL,
-      ddmodel = 11,
-      jitter = 1e-5
-    )
-  }
-
-  ml_list[[i]] <- list(
-    dna = ml_dna,
-    complete = ml_complete,
-    parameters = parameter_space[i, ]
-  )
-}
+sensitivity <- list(
+  sensitivity_dna = sensitivity_dna,
+  sensitivity_complete = sensitivity = complete
+)
 
 output_name <- "sensitivity.rds"
 
@@ -163,7 +46,7 @@ output_folder <- file.path("results")
 
 output_file_path <- file.path(output_folder, output_name)
 
-saveRDS(object = ml_list, file = output_file_path)
+saveRDS(object = sensitivity, file = output_file_path)
 
 message("Finished")
 
--- scripts/sensitivity_parallel.R
+++ scripts/sensitivity_parallel.R
@@ -1,23 +1,9 @@
+# script to run the sensitivity analysis in parallel
+
+# get argument passed from command line and store in tree_index
 args <- commandArgs(TRUE)
-
 args <- as.numeric(args)
-
 tree_index <- args[1]
-
-parameter_space <- data.frame(
-  extraction_method = "min",
-  asr_method = NA_character_,
-  tie_preference = NA_character_
-)
-
-parameter_space <- rbind(
-  parameter_space,
-  expand.grid(
-    extraction_method = c("asr"),
-    asr_method = c("parsimony", "mk"),
-    tie_preference = c("island", "mainland")
-  )
-)
 
 # load madagascar mammals species data table
 data("madagascar_mammals", package = "DAISIEprepExtra")
@@ -32,104 +18,30 @@ phylos_complete <- ape::read.nexus(file = system.file(
   package = "DAISIEprepExtra"
 ))
 
-# convert trees to phylo4 objects
-phylo_dna <- phylobase::phylo4(phylos_dna[[tree_index]])
-phylo_complete <- phylobase::phylo4(phylos_complete[[tree_index]])
-
-# create endemicity status data frame
-endemicity_status_dna <- DAISIEprep::create_endemicity_status(
-  phylo = phylo_dna,
-  island_species = madagascar_mammals
-)
-endemicity_status_complete <- DAISIEprep::create_endemicity_status(
-  phylo = phylo_complete,
-  island_species = madagascar_mammals
+sensitivity_dna <- DAISIEprep::sensitivity(
+  phylo = phylos_dna[[tree_index]],
+  island_species = madagascar_mammals,
+  extraction_method = c("min", "asr"),
+  asr_method = c("parsimony", "mk"),
+  tie_preference = c("island", "mainland"),
+  island_age = c(5),
+  num_mainland_species = c(1000)
 )
 
-# combine tree and endemicity status
-phylod_dna <- phylobase::phylo4d(
-  phylo_dna,
-  endemicity_status_dna
+sensitivity_complete <- DAISIEprep::sensitivity(
+  phylo = phylos_dna[[tree_index]],
+  island_species = madagascar_mammals,
+  extraction_method = c("min", "asr"),
+  asr_method = c("parsimony", "mk"),
+  tie_preference = c("island", "mainland"),
+  island_age = c(5),
+  num_mainland_species = c(1000)
 )
 
-phylod_complete <- phylobase::phylo4d(
-  phylo_complete,
-  endemicity_status_complete
+sensitivity <- list(
+  sensitivity_dna = sensitivity_dna,
+  sensitivity_complete = sensitivity_complete
 )
-
-ml_list <- list()
-for (i in seq_len(nrow(parameter_space))) {
-
-  message("Parameter set: ", i, " of ", nrow(parameter_space))
-
-  if (parameter_space$extraction_method[i] == "asr") {
-    phylod_dna <- DAISIEprep::add_asr_node_states(
-      phylod = phylod_dna,
-      asr_method = parameter_space$asr_method[i]
-    )
-    phylod_complete <- DAISIEprep::add_asr_node_states(
-      phylod = phylod_complete,
-      asr_method = parameter_space$asr_method[i]
-    )
-  }
-
-  # extract island community
-  island_tbl_dna <- DAISIEprep::extract_island_species(
-    phylod = phylod_dna,
-    extraction_method = parameter_space$extraction_method[i]
-  )
-  island_tbl_complete <- DAISIEprep::extract_island_species(
-    phylod = phylod_complete,
-    extraction_method = parameter_space$extraction_method[i]
-  )
-
-  # convert to daisie data table
-  daisie_datatable_dna <- DAISIEprep::as_daisie_datatable(
-    island_tbl = island_tbl_dna,
-    island_age = 88
-  )
-  daisie_datatable_complete <- DAISIEprep::as_daisie_datatable(
-    island_tbl = island_tbl_complete,
-    island_age = 88
-  )
-
-  # convert to daisie data list
-  daisie_data_list_dna <- DAISIEprep::create_daisie_data(
-    daisie_datatable = daisie_datatable_dna,
-    island_age = 88,
-    num_mainland_species = 1000
-  )
-  daisie_data_list_complete <- DAISIEprep::create_daisie_data(
-    daisie_datatable = daisie_datatable_complete,
-    island_age = 88,
-    num_mainland_species = 1000
-  )
-
-  ml_dna <- DAISIE::DAISIE_ML_CS(
-    datalist = daisie_data_list_dna,
-    initparsopt = c(1, 1, 200, 0.1, 1),
-    idparsopt = 1:5,
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    jitter = 1e-5
-  )
-
-  ml_complete <- DAISIE::DAISIE_ML_CS(
-    datalist = daisie_data_list_complete,
-    initparsopt = c(1, 1, 200, 0.1, 1),
-    idparsopt = 1:5,
-    parsfix = NULL,
-    idparsfix = NULL,
-    ddmodel = 11,
-    jitter = 1e-5
-  )
-  ml_list[[i]] <- list(
-    dna = ml_dna,
-    complete = ml_complete,
-    parameters = parameter_space[i, ]
-  )
-}
 
 output_name <- paste0("sensitivity_tree_index_", tree_index, ".rds")
 
@@ -137,6 +49,7 @@ output_folder <- file.path("results")
 
 output_file_path <- file.path(output_folder, output_name)
 
-saveRDS(object = ml_list, file = output_file_path)
+saveRDS(object = sensitivity, file = output_file_path)
 
 message("Finished")
+
