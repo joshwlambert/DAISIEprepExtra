911d7a4fed084fcb3b59e1851f665b898dfb14fb
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-23 08:18:12 +0000

    updated sensitivity script and plot

--- inst/scripts/plot_sensitivity.R
+++ inst/scripts/plot_sensitivity.R
@@ -13,154 +13,24 @@ if (length(sensitivity_data_files) == 0) {
   sensitivity_data <- lapply(file_paths, readRDS)
 }
 
-dna <- lapply(sensitivity_data, function(x) {
-  lapply(x, "[[", 1)
-})
+sensitivity_dna <- lapply(sensitivity_data, "[[", "sensitivity_dna")
+sensitivity_complete <- lapply(sensitivity_data, "[[", "sensitivity_complete")
 
-complete <- lapply(sensitivity_data, function(x) {
-  lapply(x, "[[", 2)
-})
+sensitivity_dna <- Reduce(rbind, sensitivity_dna)
+sensitivity_complete <- Reduce(rbind, sensitivity_complete)
 
-parameters <- lapply(sensitivity_data, function(x) {
-  lapply(x, "[[", 3)
-})
-
-dna <- unlist(dna, recursive = FALSE)
-dna_clado <- sapply(dna, "[[", "lambda_c")
-dna_ext <- sapply(dna, "[[", "mu")
-dna_k <- sapply(dna, "[[", "K")
-dna_immig <- sapply(dna, "[[", "gamma")
-dna_ana <- sapply(dna, "[[", "lambda_a")
-
-complete <- unlist(complete, recursive = FALSE)
-complete_clado <- sapply(complete, "[[", "lambda_c")
-complete_ext <- sapply(complete, "[[", "mu")
-complete_k <- sapply(complete, "[[", "K")
-complete_immig <- sapply(complete, "[[", "gamma")
-complete_ana <- sapply(complete, "[[", "lambda_a")
-
-parameters <- unlist(parameters, recursive = FALSE)
-extraction_method <- sapply(parameters, "[[", "extraction_method")
-asr_method <- sapply(parameters, "[[", "asr_method")
-tie_preference <- sapply(parameters, "[[", "tie_preference")
-
-plotting_data_dna <- tibble::tibble(
-  extraction_method = extraction_method,
-  asr_method = asr_method,
-  tie_preference = tie_preference,
-  dna_clado = dna_clado,
-  dna_ext = dna_ext,
-  #dna_k = dna_k,
-  dna_immig = dna_immig,
-  dna_ana = dna_ana
+sensitivity_data <- list(
+  sensitivity_dna = sensitivity_dna,
+  sensitivity_complete = sensitivity_complete
 )
 
-plotting_data_dna <- tidyr::unite(
-  data = plotting_data_dna,
-  col = extraction,
-  extraction_method:tie_preference
+sensitivity <- DAISIEprep:::plot_sensitivity(
+  sensitivity_data = sensitivity_data$sensitivity_dna
 )
-
-plotting_data_dna <- tidyr::pivot_longer(
-  data = plotting_data_dna,
-  names_to = "parameter",
-  dna_clado:dna_ana,
-  values_to = "rates"
-)
-
-# check whether data using tie_preference of island or mainland makes a
-# difference to the rates estimated from DAISIE. If they are all identical
-# they can be removed from the data as the density plots will completely overlap
-
-which_asr_mk_island <- which(
-  plotting_data_dna$extraction == "asr_mk_island"
-)
-which_asr_mk_mainland <- which(
-  plotting_data_dna$extraction == "asr_mk_mainland"
-)
-identical_mk <- identical(
-  plotting_data_dna[which_asr_mk_island, "rates"],
-  plotting_data_dna[which_asr_mk_mainland, "rates"]
-)
-
-which_asr_parsimony_island <- which(
-  plotting_data_dna$extraction == "asr_parsimony_island"
-)
-which_asr_parsimony_mainland <- which(
-  plotting_data_dna$extraction == "asr_parsimony_mainland"
-)
-identical_parsimony <- identical(
-  plotting_data_dna[which_asr_parsimony_island, "rates"],
-  plotting_data_dna[which_asr_parsimony_mainland, "rates"]
-)
-
-
-if (identical_mk && identical_parsimony) {
-  plotting_data_dna <- dplyr::slice(
-    plotting_data_dna,
-    -which(
-      plotting_data_dna$extraction %in%
-        c("asr_mk_mainland", "asr_parsimony_mainland")
-    )
-  )
-
-  sensitivity <- ggplot2::ggplot(plotting_data_dna) +
-    ggplot2::geom_density(
-      mapping = ggplot2::aes(rates, fill = extraction),
-      alpha = 0.5
-    ) +
-    ggplot2::scale_x_continuous(name = "Rate") +
-    ggplot2::scale_y_continuous(name = "Density") +
-    ggplot2::scale_fill_discrete(
-      name = "Extraction Method",
-      type = c("#FF0000", "#00A08A", "#F2AD00", "#F98400", "#5BBCD6"),
-      labels = c("min", "ASR (parsimony)", "ASR (Mk)")) +
-    ggplot2::theme_classic() +
-    ggplot2::facet_wrap(
-      facets = "parameter",
-      scales = "free",
-      labeller = ggplot2::as_labeller(c(
-        dna_ana = "Anagenesis",
-        dna_clado = "Cladogenesis",
-        dna_ext = "Extinction",
-        dna_immig =  "Colonisation")
-      )
-    ) +
-    ggplot2::theme(strip.background = ggplot2::element_blank(),
-                   strip.text = ggtext::element_markdown())
-} else {
-
-  sensitivity <- ggplot2::ggplot(plotting_data_dna) +
-    ggplot2::geom_density(
-      mapping = ggplot2::aes(rates, fill = extraction),
-      alpha = 0.5
-    ) +
-    ggplot2::scale_x_continuous(name = "Rate") +
-    ggplot2::scale_y_continuous(name = "Density") +
-    ggplot2::scale_fill_discrete(
-      name = "Extraction Method",
-      type = c("#FF0000", "#00A08A", "#F2AD00", "#F98400", "#5BBCD6"),
-      labels = labels = c("ASR (Mk island)", "ASR (Mk mainland)",
-                          "ASR (parsimony island)", "ASR (parsimony mainland)",
-                          "min")) +
-    ggplot2::theme_classic() +
-    ggplot2::facet_wrap(
-      facets = "parameter",
-      scales = "free",
-      labeller = ggplot2::as_labeller(c(
-        dna_ana = "Anagenesis",
-        dna_clado = "Cladogenesis",
-        dna_ext = "Extinction",
-        dna_immig =  "Colonisation")
-      )
-    ) +
-    ggplot2::theme(strip.background = ggplot2::element_blank(),
-                   strip.text = ggtext::element_markdown())
-}
 
 ggplot2::ggsave(
   plot = sensitivity,
-  filename = file.path("plots", "sensitivity.png"),
+  filename = file.path("inst", "plots", "sensitivity.png"),
   device = "png",
   width = 300,
   height = 200,
