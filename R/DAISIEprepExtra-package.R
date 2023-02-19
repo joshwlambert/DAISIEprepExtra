1ddac08db776af2405338ac4c35890f54a2fc84a
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-02-19 13:33:51 +0000

    added imports to namespace and package file to remove check note

---
+++ R/DAISIEprepExtra-package.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Suppress R CMD check note
+## usethis namespace: start
+#' @importFrom cowplot plot_grid
+#' @importFrom DAISIE DAISIE_ML_CS
+#' @importFrom DAISIEprep extract_island_species
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggspatial geom_sf
+#' @importFrom ggtree ggtree
+#' @importFrom phylobase phylo4
+#' @importFrom remotes install_github
+#' @importFrom rnaturalearth ne_countries
+#' @importFrom sf as_Spatial
+#' @importFrom tibble tibble
+#' @importFrom TreeSim sim.bd.taxa
+## usethis namespace: end
+NULL
